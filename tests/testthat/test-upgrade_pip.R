19a6685cb054b32dd1291032dd5ba5e67259c84e
richelbilderbeek <richel@richelbilderbeek.nl>
2021-12-17 19:09:08 +0100

    Rely on 'ormr'

--- tests/testthat/test-upgrade_pip.R
+++
@@ -1,9 +0,0 @@
-test_that("use", {
-  skip("Use 'ormr'")
-  if (!mhcnuggetsr::is_on_ci()) return()
-  if (!mhcnuggetsr::is_pip_installed()) return()
-
-  # Does nothing, expect to be at the most current version
-  expect_silent(upgrade_pip())
-
-})
